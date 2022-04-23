eb18f3583abc5544516b7e5b671967d5d7d8801e
Charon Wang <charonwangg@Gmail.com>
2022-04-22 21:07:22 -0400

    Allow hyperparameters change

--- turtle_soup_boiler/boiler.py
+++ turtle_soup_boiler/boiler.py
@@ -3,23 +3,24 @@ import pickle
 
 
 class TurtleSoupBoiler:
-    def __init__(self, filename=None):
+    def __init__(self, num_sent=5, p_sample=0.6, sample_step=2, filename=None):
         configure_openai()
+        self.__dict__.update(locals())
         self.story = []
-        if not filename:
+        if filename is None:
             self.filename = 'story.pkl'
 
     # Generate a story by input a sentence and store it in self.story
     def generate_by_input(self):
         print('>Please input the first sentence of your story:')
         sentence = input()
-        story = generate_story(sentence)
+        story = generate_story(sentence, self.num_sent, self.p_sample, self.sample_step)
         self.story.append(story)
         print('>Generate story successfully!')
 
     # Generate a story by string and store it in self.story
     def generate_by_text(self, text):
-        story = generate_story(text)
+        story = generate_story(text, self.num_sent, self.p_sample, self.sample_step)
         self.story.append(story)
         print('>Generate story successfully!')
 
